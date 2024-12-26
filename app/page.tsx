7a668028fdfd4cf51ed1a633ca874082c63cdff8
Hakashi Katake <gammlast@gmail.com>
2024-12-26 20:04:17 +0530

    guide section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Camp from "@/components/Camp";
 import GetApp from "@/components/GetApp";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
-import Guide from "@/components/Guid";
+import Guide from "@/components/Guide";
 import Image from "next/image";
 
 export default function Home() {
--- components/Guid.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Guid = () => {
-  return (
-    <div>Guid</div>
-  )
-}
-
-export default Guid
